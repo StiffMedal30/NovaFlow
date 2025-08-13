391d15f31b9b1105f4c6503736bf12aa3e0f2da4
Christiaan Dotze <christiaandotze@gmail.com>
2025-08-13 13:37:46 +0200

    added feign client to send requests to ai service

--- src/main/java/za/co/idea/service/IdeaServiceApplication.java
+++ src/main/java/za/co/idea/service/IdeaServiceApplication.java
@@ -2,8 +2,10 @@ package za.co.idea.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class IdeaServiceApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/za/co/idea/service/client/AiServiceClient.java
@@ -0,0 +1,16 @@
+package za.co.idea.service.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import za.co.idea.service.records.AiServiceResponse;
+import za.co.idea.service.records.IdeaRecord;
+
+@FeignClient(name = "ai-service")
+public interface AiServiceClient {
+
+    @PostMapping("/api/ai/process")
+    AiServiceResponse processIdea(@RequestBody IdeaRecord idea);
+
+}
+
